05d434727c63baa44cacb28359f0adcaf279c795
AlejandroRiveraH <alejandrojoseriveraherrera@gmail.com>
2021-04-28 04:54:48 +0000

    Creacion de la tabla de usuarios, campos email, password, age, gender, country, mascot

--- src/api/models.py
+++ src/api/models.py
@@ -3,10 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__= 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    age = db.Column(db.Integer, unique=False, nullable=False)
+    gender = db.Column(db.String(120), unique=False, nullable=False)
+    country = db.Column(db.String(120), unique=False, nullable=False)
+    #is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +21,8 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    #user = User( email = <email_value>, password = <username_value>)
+    #db.session.add(user)
+    #db.session.commit()    
+
